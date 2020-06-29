4895e4a38d42f3213e23af148997aec2d896f99b
soetje <imredesoet@hotmail.com>
2020-06-29 16:39:57 +0200

    interpolated array

--- src/App.js
+++ src/App.js
@@ -2,46 +2,47 @@ import React from "react"
 import Title from "./components/Title"
 import Pokemon from "./components/Pokemon"
 
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"]
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"]
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"]
+  }
+];
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <main>
-        <Title content="React is fun" />
-        <Pokemon
-          name = "Charrizard"
-          weight = "90"
-          awesome = {false}
-          terrifying = {false}
-          abillities = {'Blaze, Solar power, Though claws, Drought'}
-        />
-        <Pokemon
-          name = "Bulbasaur"
-          weight = "6.9"
-          awesome = {true}
-          terrifying = {false}
-          abilities = {"Overgrow, Chlorophyll"}
-        />
-         <Pokemon
-          name = "Mewtwo"
-          weight = "122"
-          awesome = {true}
-          terrifying = {true}
-          abilities = {"Pressure, Unnerve, Steadfast, Insomnia"}
-        />
-         <Pokemon
-          name = "Mega beedrill"
-          weight = "65"
-          awesome = {false}
-          terrifying = {true}
-          abilities = {"intimidate, Unnerve"}
-        />
+        <Title content="Awesome App React" />
+        {all_pokemon.map(Pokemon)}
       </main>
-
-      <header>
-        <p>welcome to Codaisseur.</p>
-      </header>
     </div>
-  )
+  );
 }
+
 
 export default App
