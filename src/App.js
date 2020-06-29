2e5bed22b4b4f82110d78ed5d94d514655506d5f
soetje <imredesoet@hotmail.com>
2020-06-29 18:38:43 +0200

    styling css bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Title from "./components/Title"
-import Pokemon from "./components/Pokemon"
+import Pokemon from "./components/Pokemon/Pokemon"
 
 const all_pokemon = [
   {
@@ -8,41 +8,50 @@ const all_pokemon = [
     weight: 90,
     awesome: true,
     terrifying: false,
-    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"]
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
   },
   {
     name: "Bulbasaur",
     weight: 6.9,
     awesome: true,
     terrifying: false,
-    abilities: ["Overgrow", "Chlorophyll"]
+    abilities: ["Overgrow", "Chlorophyll"],
   },
   {
     name: "Mewtwo",
     weight: 122,
     awesome: true,
     terrifying: true,
-    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
   },
   {
     name: "Mega beedrill",
     weight: 65,
     awesome: false,
     terrifying: true,
-    abilities: ["Intimidate", "Unnerve"]
-  }
-];
+    abilities: ["Intimidate", "Unnerve"],
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <Title content="Awesome App React" />
-        {all_pokemon.map(Pokemon)}
-      </main>
-    </div>
-  );
+    <main className="container my-5">
+      <Title content="Awesome App React" />
+      <div className="row">
+        {all_pokemon.map((pokemon) => (
+          <div className="col-md-6 col-lg-4">
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
-
 
 export default App
---
+++ src/components/Pokemon/Pokemon.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./Pokemon.scss"
+import "./Pokemon.css"
+
+export default function Pokemon(props) {
+  return (
+    
+    <div className="card shadow-sm mb-4 Pokemon">
+      <div class="element_container">
+<div class="rotate_me"></div>
+</div>
+      <div class="card-body pb-0">
+        <h5 class="card-title ">{props.name}</h5>
+        <h6 class="card-subtitle mb-3 text-primary">
+          {props.awesome ? "An awesome Pokemon" : "Not awesome"}
+        </h6>
+        <p className="mb-0">
+          Weight: {props.weight} kg
+          <br />
+          Terrifying: {props.terrifying ? "Very" : "nah, lovable"}
+          <br />
+          {props.abilities.length} abilities
+        </p>
+      </div>
+      <ul className="list-group list-group-flush">
+        {props.abilities.map((ability) => {
+          return <li className="list-group-item">{ability}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
